8264ee331ff7f8bdf8599b6f76e15cfc249e2339
João Maia <15joaomaia@gmail.com>
2022-02-20 16:03:37 -0300

    adicionando animaçoes

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -1,24 +1,27 @@
 let menu = document.getElementById('icon-menu')
 
-
-
 menu.addEventListener('click', ()=>{
+
     let element = document.querySelectorAll('li')
 
-    let iconPlus = document.getElementById('icon-plus')
+    // let iconPlus = document.getElementById('icon-plus')
 
     let sidebar = document.getElementById('sidebar')
 
-    if(sidebar.style.width == '200px'){
-        sidebar.style.width = '36px'
+    if(sidebar.style.width == '36px'){
+        sidebar.style.width = '200px'
+        sidebar.classList.add('animation')
+        iconPlus.classList.remove('none')
         element.forEach((e)=>{
-            e.classList.add('none')
+            e.classList.add('block')
         })
     }else{
+        sidebar.style.width = '36px'
+        iconPlus.classList.add('none')
+        sidebar.classList.add('animation')
         element.forEach((e)=>{
-            e.classList.remove('none')
+            e.classList.remove('block')
         })
-        sidebar.style.width = '200px'
     }
     console.log(element)
     
