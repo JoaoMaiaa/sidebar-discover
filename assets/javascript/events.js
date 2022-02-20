1ace21d5f2822065ae3127b7ac3a6aefb27031eb
João Maia <15joaomaia@gmail.com>
2022-02-20 19:37:27 -0300

    tentando adicionar transition nos elementos

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -4,25 +4,31 @@ menu.addEventListener('click', ()=>{
 
     let element = document.querySelectorAll('li')
 
-    // let iconPlus = document.getElementById('icon-plus')
+    let iconPlus = document.getElementById('icon-plus')
 
     let sidebar = document.getElementById('sidebar')
 
     if(sidebar.style.width == '36px'){
+
         sidebar.style.width = '200px'
-        sidebar.classList.add('animation')
         iconPlus.classList.remove('none')
+        sidebar.classList.add('animation')
+
         element.forEach((e)=>{
-            e.classList.add('block')
+            e.classList.remove('none')
+            e.classList.add('animation')
         })
+
     }else{
+
         sidebar.style.width = '36px'
         iconPlus.classList.add('none')
         sidebar.classList.add('animation')
         element.forEach((e)=>{
-            e.classList.remove('block')
+            e.classList.add('none')
+            e.classList.add('animation')
         })
+
     }
-    console.log(element)
     
 })
